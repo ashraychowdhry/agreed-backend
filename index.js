34d2a7576c2f35f6ca7aa46b247de2404a23216d
Ashray Chowdhry <ashraychowdhry@gmail.com>
2021-11-16 16:29:32 -0500

    local push

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ mongoose.connect('mongodb+srv://general-access:<qaz1wsx2>@cluster0.mhu4h.mongodb
 
 const port = 3001
 
+
+
 app.use(express.json())
 
 app.get('/health', (req, res) => {
     //res.send('HEALTH: OK')
     res.json({status: 'ok'})
+})
+
+app.get('/test', (req, res) => {
+
+
 })
 
 app.listen(port, () => {
