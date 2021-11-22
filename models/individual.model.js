111906ec9a06261c45d153e981879c0819a555fe
Gayathri Nandyalam <gnandyalam@lawn-128-61-112-92.lawn.gatech.edu>
2021-11-21 20:35:21 -0500

    individual form backend functionality

---
+++ models/individual.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const Individual = new mongoose.Schema({
+    budget: {type: String, required: true},
+    arrival: {type: String, required: true},
+    departure: {type: String, required: true},
+    arrivalTime: {type: String, required: true},
+    departureTime: {type: String, required: true},
+    flightPreference: {type: String, required: true},
+}, {
+    collection: 'individual-data'
+})
+
+const model = mongoose.model('Individual', Individual)
+module.exports = model
