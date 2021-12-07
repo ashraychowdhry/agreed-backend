2bf8161eb655e30be501bb83e2cb7ee2feea53a8
Ashray Chowdhry <ashraychowdhry@gmail.com>
2021-12-07 12:53:06 -0500

    registration fix

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post("api/payment", async (req, res) => {
 	}
 })
 
-app.post('/register', async (req, res) => {
+app.post('/api/register', async (req, res) => {
     console.log(req.body)
     try {
         await User.create({
